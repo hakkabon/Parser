d6269bb4e7c1366be172565f19707e7a9de54313
Ulf Akerstedt-Inoue <hakkabon@gmail.com>
2026-09-03 12:51:27 +0900

    Adopt tagged package dependencies

--- Package.swift
+++ Package.swift
@@ -12,9 +12,9 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/hakkabon/Grammar.git",
-            revision: "69f85d7a493e1862412c34493e3656e94331df06"
+            .upToNextMinor(from: "0.2.0")
         ),
-        .package(url: "https://github.com/hakkabon/TerminalColors.git", from: "0.0.1"),
+        .package(url: "https://github.com/hakkabon/TerminalColors.git", .upToNextMinor(from: "0.1.0")),
     ],
     targets: [
         .target(
